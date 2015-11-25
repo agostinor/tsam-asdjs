adf4409de93b1b72e745ba3fca6a551ee0acbc5b
Agostino <agostino.romanelli@gmail.com>
2015-11-25 14:56:14 +0100

    modifica

--- homework_04.js
+++ homework_04.js
@@ -1,11 +1,11 @@
-function dec2bin01(n){
+/*function dec2bin01(n){
     output = "";
     for(i = n; i > 0; i = (i - rest)/2){
         rest = i%2
         output = output + "" + rest + "";
     }
     return output;
-}
+}*/
 
 function Stack(){
     this.myarray = []
@@ -16,7 +16,7 @@ Stack.prototype.pop = function(){ return this.myarray.pop(); }
 Stack.prototype.peek = function(){ return this.myarray[this.myarray.length-1]; }
 Stack.prototype.isEmpty = function() { return this.myarray.length == 0; }
 
-function dec2bin3(x){
+function dec2bin(x){
 
     function rec(remStack, n) {
         if (n == 0) {
